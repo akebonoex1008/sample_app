7aaf0892f5a4ff4a5a61c55b5167fe5fb5464300
akebono <tx.yagi@gmail.com>
2019-09-25 13:57:39 +0900

    image upload

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,20 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
-  validates :user_id, presence: true #マイクロポストのuser_idに対する検証
+  mount_uploader :picture, PictureUploader        # Micropostモデルに画像を追加する
+  validates :user_id, presence: true              #マイクロポストのuser_idに対する検証
   validates :content, presence: true, length: { maximum: 140 } # Micropostモデルのバリデーション
+  validate  :picture_size
+
+
+  #######
+  private
+  #######
+
+  # アップロードされた画像の左図をバリデーションする
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
